34fb317e23d0e7d93630b25195c3a25ae22347f1
C.W. Betts <computers57@hotmail.com>
2015-09-09 14:38:44 -0600

    Migrate most of the code to Swift. Just because.

--- Source/QuickLookClass.swift
+++ Source/QuickLookClass.swift
@@ -11,52 +11,164 @@ import CoreServices
 import QuickLook
 import WebKit
 
+// 5E2D9680-5022-40FA-B806-43349622E5B9
+private let kQLGeneratorTypeID = CFUUIDGetConstantUUIDWithBytes(kCFAllocatorDefault, 0x5E, 0x2D, 0x96, 0x80, 0x50, 0x22, 0x40, 0xFA, 0xB8, 0x06, 0x43, 0x34, 0x96, 0x22, 0xE5, 0xB9)
+// 865AF5E0-6D30-4345-951B-D37105754F2D
+private let kQLGeneratorCallbacksInterfaceID = CFUUIDGetConstantUUIDWithBytes(kCFAllocatorDefault, 0x86, 0x5A, 0xF5, 0xE0, 0x6D, 0x30, 0x43, 0x45, 0x95, 0x1B, 0xD3, 0x71, 0x05, 0x75, 0x4F, 0x2D)
+
+// Don't modify this line
+private let PLUGIN_ID = "984AED87-72B9-4060-B7BC-935561C2221B"
+
+
+private let S_OK: HRESULT = 0
+private let E_NOINTERFACE = HRESULT(bitPattern: 0x80000004)
+
 /// The minimum aspect ratio (width / height) of a thumbnail.
 private let MINIMUM_ASPECT_RATIO: CGFloat = 1.0 / 2.0
 
+struct QLGeneratorPlugType {
+    var conduitInterface: UnsafeMutablePointer<()>
+    var factoryID: CFUUIDRef!
+    var refCount: UInt32
+}
+
+var myInterfaceFtbl = QLGeneratorInterfaceStruct(_reserved: nil,
+    QueryInterface: QuickLookGeneratorQueryInterface,
+    AddRef: QuickLookGeneratorPluginAddRef,
+    Release: QuickLookGeneratorPluginRelease,
+    GenerateThumbnailForURL: nil,
+    CancelThumbnailGeneration: nil,
+    GeneratePreviewForURL: nil,
+    CancelPreviewGeneration: nil)
+
+///Implementation of the `IUnknown` QueryInterface function.
+func QuickLookGeneratorQueryInterface(thisInstance: UnsafeMutablePointer<Void>, iid: REFIID, ppv: UnsafeMutablePointer<LPVOID>) -> HRESULT {
+    let interfaceID = CFUUIDCreateFromUUIDBytes(kCFAllocatorDefault, iid)!
+    
+    if CFEqual(interfaceID, kQLGeneratorCallbacksInterfaceID) {
+        /* If the Right interface was requested, bump the ref count,
+        * set the ppv parameter equal to the instance, and
+        * return good status.
+        */
+
+        let tmpConInterface: UnsafeMutablePointer<QLGeneratorInterfaceStruct> = {
+            let tmpInterface = UnsafeMutablePointer<QLGeneratorPlugType>(thisInstance)
+            return UnsafeMutablePointer<QLGeneratorInterfaceStruct>(tmpInterface.memory.conduitInterface)
+            }()
+        
+        tmpConInterface.memory.GenerateThumbnailForURL = generateThumbnail
+        tmpConInterface.memory.CancelThumbnailGeneration = cancelThumbnailGeneration
+        tmpConInterface.memory.GeneratePreviewForURL = generatePreview
+        tmpConInterface.memory.CancelPreviewGeneration = cancelPreviewGeneration
+
+        tmpConInterface.memory.AddRef(thisInstance)
+        //QLGeneratorPlugType
+        ppv.memory = thisInstance
+        return S_OK
+    } else {
+        ppv.memory = nil
+        return E_NOINTERFACE
+    }
+}
+
+///Implementation of reference counting for this type. Whenever an interface
+///is requested, bump the refCount for the instance. NOTE: returning the
+///refcount is a convention but is not required so don't rely on it.
+func QuickLookGeneratorPluginAddRef(thisInstance: UnsafeMutablePointer<Void>) -> ULONG {
+    let tmpInstance = UnsafeMutablePointer<QLGeneratorPlugType>(thisInstance)
+    return ++tmpInstance.memory.refCount
+}
+
+///When an interface is released, decrement the refCount.<br>
+///If the refCount goes to zero, deallocate the instance.
+func QuickLookGeneratorPluginRelease(thisInstance: UnsafeMutablePointer<Void>) -> ULONG {
+    let anInstance = UnsafeMutablePointer<QLGeneratorPlugType>(thisInstance)
+    anInstance.memory.refCount -= 1
+    if anInstance.memory.refCount == 0 {
+        DeallocQuickLookGeneratorPluginType(anInstance)
+        return 0;
+    } else {
+        return anInstance.memory.refCount
+    }
+}
+
+
+func DeallocQuickLookGeneratorPluginType(thisInstance: UnsafeMutablePointer<QLGeneratorPlugType>) {
+    let theFactoryID = thisInstance.memory.factoryID
+    thisInstance.memory.factoryID = nil
+    
+    /* Free the conduitInterface table up */
+    free(thisInstance.memory.conduitInterface);
+    
+    /* Free the instance structure */
+    free(thisInstance);
+    if let theFactoryID = theFactoryID {
+        CFPlugInRemoveInstanceForFactory(theFactoryID);
+    }
+}
+
+func generatePreview(thisInstance: UnsafeMutablePointer<Void>, preview: QLPreviewRequest!, url: CFURL!, contentTypeUTI: CFString!, options: CFDictionary!) -> OSStatus {
+    if let data = renderMarkdown(url) {
+        QLPreviewRequestSetDataRepresentation(preview, data, kUTTypeHTML, [:])
+    }
+    return noErr
+}
+
+func cancelPreviewGeneration(thisInstance: UnsafeMutablePointer<Void>, preview: QLPreviewRequest!) {
+    // Implement only if supported
+}
+
+func generateThumbnail(thisInstance: UnsafeMutablePointer<Void>, thumbnail: QLThumbnailRequest!, url: CFURL!, contentTypeUTI: CFString!, options: CFDictionary!, maxSize: CGSize) -> OSStatus {
+    if let data = renderMarkdown(url) {
+        let viewRect = NSRect(x: 0, y: 0, width: 600, height: 800)
+        let scale = maxSize.height / 800.0
+        let scaleSize = NSSize(width: scale, height: scale)
+        let thumbSize = NSSize(width: maxSize.width * (600.0 / 800.0), height: maxSize.height)
+        
+        let webView = WebView(frame: viewRect)
+        webView.scaleUnitSquareToSize(scaleSize)
+        webView.mainFrame.frameView.allowsScrolling = false
+        webView.mainFrame.loadData(data, MIMEType: "text/html", textEncodingName: "utf-8", baseURL: nil)
+        
+        while webView.loading {
+            CFRunLoopRunInMode(kCFRunLoopDefaultMode, 0, true)
+        }
+        
+        webView.display()
+        
+        if let context = QLThumbnailRequestCreateContext(thumbnail, thumbSize, false, nil)?.takeRetainedValue() {
+            let nsContext = NSGraphicsContext(CGContext: context, flipped: webView.flipped)
+            webView.displayRectIgnoringOpacity(webView.bounds, inContext: nsContext)
+            
+            QLThumbnailRequestFlushContext(thumbnail, context);
+        }
+    }
+    
+    return noErr;
+}
+
+func cancelThumbnailGeneration(thisInstance: UnsafeMutablePointer<Void>, thumbnail: QLThumbnailRequest!) {
+    // Implement only if supported
+}
 
 final class QLMarkDownGenerator: NSObject {
-	@objc(generatePreview:forURL:contentTypeUTI:options:) class func generatePreview(preview: QLPreviewRequest, url: CFURL, contentTypeUTI: CFString, options: CFDictionary) -> OSStatus {
-		if let data = renderMarkdown(url) {
-            QLPreviewRequestSetDataRepresentation(preview, data, kUTTypeHTML, [:])
-		}
-		return noErr
-	}
-	
-	@objc class func cancelPreviewGeneration(preview: QLPreviewRequest) {
-		// Implement only if supported
-	}
-	
-	@objc(generateThumbnail:forURL:contentTypeUTI:options:maxSize:) class func generateThumbnail(thumbnail: QLThumbnailRequest, url: CFURL, contentTypeUTI: CFString, options: CFDictionary, maxSize: CGSize) -> OSStatus {
-		if let data = renderMarkdown(url) {
-			let viewRect = NSRect(x: 0, y: 0, width: 600, height: 800)
-			let scale = maxSize.height / 800.0
-			let scaleSize = NSSize(width: scale, height: scale)
-			let thumbSize = NSSize(width: maxSize.width * (600.0 / 800.0), height: maxSize.height)
-			
-			let webView = WebView(frame: viewRect)
-			webView.scaleUnitSquareToSize(scaleSize)
-			webView.mainFrame.frameView.allowsScrolling = false
-			webView.mainFrame.loadData(data, MIMEType: "text/html", textEncodingName: "utf-8", baseURL: nil)
-			
-			while webView.loading {
-				CFRunLoopRunInMode(kCFRunLoopDefaultMode, 0, true)
-			}
-			
-			webView.display()
-			
-			if let context = QLThumbnailRequestCreateContext(thumbnail, thumbSize, false, nil)?.takeRetainedValue() {
-				let nsContext = NSGraphicsContext(CGContext: context, flipped: webView.flipped)
-				webView.displayRectIgnoringOpacity(webView.bounds, inContext: nsContext)
-				
-				QLThumbnailRequestFlushContext(thumbnail, context);
-			}
-		}
-		
-		return noErr;
-	}
-	
-	@objc class func cancelThumbnailGeneration(thumbnail: QLThumbnailRequest) {
-		// Implement only if supported
-	}
+    
+    /// Utility function that allocates a new instance.<br>
+    ///      You can do some initial setup for the generator here if you wish
+    ///      like allocating globals etc...
+    class func allocQuickLookGeneratorPluginType(inFactoryID: CFUUID) -> UnsafeMutablePointer<()> {
+        let theNewInstance = UnsafeMutablePointer<QLGeneratorPlugType>(calloc(sizeof(QLGeneratorPlugType), 1))
+        
+        /* Point to the function table Malloc enough to store the stuff and copy the filler from myInterfaceFtbl over */
+        theNewInstance.memory.conduitInterface = malloc(sizeof(QLGeneratorInterfaceStruct))
+        memcpy(theNewInstance.memory.conduitInterface, &myInterfaceFtbl, sizeof(QLGeneratorInterfaceStruct))
+        
+        /*  Retain and keep an open instance refcount for each factory. */
+        theNewInstance.memory.factoryID = inFactoryID
+        CFPlugInAddInstanceForFactory(inFactoryID)
+        
+        /* This function returns the IUnknown interface so set the refCount to one. */
+        theNewInstance.memory.refCount = 1;
+        return UnsafeMutablePointer<()>(theNewInstance)
+    }
 }
--- main.m
+++ main.m
@@ -9,9 +9,6 @@
 
 
 
-
-
-
 #include <CoreFoundation/CoreFoundation.h>
 #include <CoreFoundation/CFPlugInCOM.h>
 #include <CoreServices/CoreServices.h>
@@ -27,219 +24,23 @@
 // Don't modify this line
 #define PLUGIN_ID "984AED87-72B9-4060-B7BC-935561C2221B"
 
-//
-// Below is the generic glue code for all plug-ins.
-//
-// You should not have to modify this code aside from changing
-// names if you decide to change the names defined in the Info.plist
-//
-
-
-// -----------------------------------------------------------------------------
-//	typedefs
-// -----------------------------------------------------------------------------
-
-// The thumbnail generation function to be implemented in GenerateThumbnailForURL.c
-static OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thumbnail, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options, CGSize maxSize);
-static void CancelThumbnailGeneration(void* thisInterface, QLThumbnailRequestRef thumbnail);
-
-// The preview generation function to be implemented in GeneratePreviewForURL.c
-static OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options);
-static void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview);
-
-// The layout for an instance of QuickLookGeneratorPlugIn
-typedef struct __QuickLookGeneratorPluginType
-{
-    void        *conduitInterface;
-    CFUUIDRef    factoryID;
-    UInt32       refCount;
-} QLGeneratorPlugType;
-
-// -----------------------------------------------------------------------------
-//	prototypes
-// -----------------------------------------------------------------------------
-//	Forward declaration for the IUnknown implementation.
-//
-
-static QLGeneratorPlugType	*AllocQuickLookGeneratorPluginType(CFUUIDRef inFactoryID);
-static void					 DeallocQuickLookGeneratorPluginType(QLGeneratorPlugType *thisInstance);
-static HRESULT				 QuickLookGeneratorQueryInterface(void *thisInstance,REFIID iid,LPVOID *ppv);
-extern void					*QuickLookGeneratorPluginFactory(CFAllocatorRef allocator,CFUUIDRef typeID);
-static ULONG				 QuickLookGeneratorPluginAddRef(void *thisInstance);
-static ULONG				 QuickLookGeneratorPluginRelease(void *thisInstance);
-
-// -----------------------------------------------------------------------------
-//	myInterfaceFtbl	definition
-// -----------------------------------------------------------------------------
-//	The QLGeneratorInterfaceStruct function table.
-//
-static QLGeneratorInterfaceStruct myInterfaceFtbl = {
-    NULL,
-    QuickLookGeneratorQueryInterface,
-    QuickLookGeneratorPluginAddRef,
-    QuickLookGeneratorPluginRelease,
-    NULL,
-    NULL,
-    NULL,
-    NULL
-};
-
-
-// -----------------------------------------------------------------------------
-//	AllocQuickLookGeneratorPluginType
-// -----------------------------------------------------------------------------
-//	Utility function that allocates a new instance.
-//      You can do some initial setup for the generator here if you wish
-//      like allocating globals etc...
-//
-QLGeneratorPlugType *AllocQuickLookGeneratorPluginType(CFUUIDRef inFactoryID)
-{
-    QLGeneratorPlugType *theNewInstance;
-
-    theNewInstance = (QLGeneratorPlugType *)calloc(sizeof(QLGeneratorPlugType), 1);
-
-    /* Point to the function table Malloc enough to store the stuff and copy the filler from myInterfaceFtbl over */
-    theNewInstance->conduitInterface = malloc(sizeof(QLGeneratorInterfaceStruct));
-    memcpy(theNewInstance->conduitInterface,&myInterfaceFtbl,sizeof(QLGeneratorInterfaceStruct));
-
-    /*  Retain and keep an open instance refcount for each factory. */
-    theNewInstance->factoryID = CFRetain(inFactoryID);
-    CFPlugInAddInstanceForFactory(inFactoryID);
-
-    /* This function returns the IUnknown interface so set the refCount to one. */
-    theNewInstance->refCount = 1;
-    return theNewInstance;
-}
-
-// -----------------------------------------------------------------------------
-//	DeallocQuickLookGeneratorPluginType
-// -----------------------------------------------------------------------------
-//	Utility function that deallocates the instance when
-//	the refCount goes to zero.
-//      In the current implementation generator interfaces are never deallocated
-//      but implement this as this might change in the future
-//
-void DeallocQuickLookGeneratorPluginType(QLGeneratorPlugType *thisInstance)
-{
-    CFUUIDRef theFactoryID = thisInstance->factoryID;
-    /* Free the conduitInterface table up */
-    free(thisInstance->conduitInterface);
-
-    /* Free the instance structure */
-    free(thisInstance);
-    if (theFactoryID){
-        CFPlugInRemoveInstanceForFactory(theFactoryID);
-        CFRelease(theFactoryID);
-    }
-}
-
-// -----------------------------------------------------------------------------
-//	QuickLookGeneratorQueryInterface
-// -----------------------------------------------------------------------------
-//	Implementation of the IUnknown QueryInterface function.
-//
-HRESULT QuickLookGeneratorQueryInterface(void *thisInstance,REFIID iid,LPVOID *ppv)
-{
-    CFUUIDRef interfaceID = CFUUIDCreateFromUUIDBytes(kCFAllocatorDefault,iid);
-
-    if (CFEqual(interfaceID, kQLGeneratorCallbacksInterfaceID)){
-        /* If the Right interface was requested, bump the ref count,
-         * set the ppv parameter equal to the instance, and
-         * return good status.
-         */
-        ((QLGeneratorInterfaceStruct *)((QLGeneratorPlugType *)thisInstance)->conduitInterface)->GenerateThumbnailForURL = GenerateThumbnailForURL;
-        ((QLGeneratorInterfaceStruct *)((QLGeneratorPlugType *)thisInstance)->conduitInterface)->CancelThumbnailGeneration = CancelThumbnailGeneration;
-        ((QLGeneratorInterfaceStruct *)((QLGeneratorPlugType *)thisInstance)->conduitInterface)->GeneratePreviewForURL = GeneratePreviewForURL;
-        ((QLGeneratorInterfaceStruct *)((QLGeneratorPlugType *)thisInstance)->conduitInterface)->CancelPreviewGeneration = CancelPreviewGeneration;
-        ((QLGeneratorInterfaceStruct *)((QLGeneratorPlugType*)thisInstance)->conduitInterface)->AddRef(thisInstance);
-        *ppv = thisInstance;
-        CFRelease(interfaceID);
-        return S_OK;
-    }else{
-        /* Requested interface unknown, bail with error. */
-        *ppv = NULL;
-        CFRelease(interfaceID);
-        return E_NOINTERFACE;
-    }
-}
-
-// -----------------------------------------------------------------------------
-// QuickLookGeneratorPluginAddRef
-// -----------------------------------------------------------------------------
-//	Implementation of reference counting for this type. Whenever an interface
-//	is requested, bump the refCount for the instance. NOTE: returning the
-//	refcount is a convention but is not required so don't rely on it.
-//
-ULONG QuickLookGeneratorPluginAddRef(void *thisInstance)
-{
-    return ++((QLGeneratorPlugType *)thisInstance )->refCount;
-}
-
-// -----------------------------------------------------------------------------
-// QuickLookGeneratorPluginRelease
-// -----------------------------------------------------------------------------
-//	When an interface is released, decrement the refCount.
-//	If the refCount goes to zero, deallocate the instance.
-//
-ULONG QuickLookGeneratorPluginRelease(void *thisInstance)
-{
-    ((QLGeneratorPlugType*)thisInstance)->refCount -= 1;
-    if (((QLGeneratorPlugType*)thisInstance)->refCount == 0){
-        DeallocQuickLookGeneratorPluginType((QLGeneratorPlugType*)thisInstance );
-        return 0;
-    }else{
-        return ((QLGeneratorPlugType*) thisInstance )->refCount;
-    }
-}
-
 // -----------------------------------------------------------------------------
 //  QuickLookGeneratorPluginFactory
 // -----------------------------------------------------------------------------
 void *QuickLookGeneratorPluginFactory(CFAllocatorRef allocator,CFUUIDRef typeID)
 {
-    QLGeneratorPlugType	*result;
-    CFUUIDRef			uuid;
+    void        *result;
+    CFUUIDRef    uuid;
 
     /* If correct type is being requested, allocate an
      * instance of kQLGeneratorTypeID and return the IUnknown interface.
      */
     if (CFEqual(typeID, kQLGeneratorTypeID)){
         uuid = CFUUIDCreateFromString(kCFAllocatorDefault, CFSTR(PLUGIN_ID));
-        result = AllocQuickLookGeneratorPluginType(uuid);
+        result = [QLMarkDownGenerator allocQuickLookGeneratorPluginType:uuid];
         CFRelease(uuid);
         return result;
     }
     /* If the requested type is incorrect, return NULL. */
     return NULL;
-}
-
-OSStatus GenerateThumbnailForURL(void *thisInterface,
-								 QLThumbnailRequestRef thumbnail,
-								 CFURLRef url, CFStringRef contentTypeUTI,
-								 CFDictionaryRef options, CGSize maxSize)
-{
-    @autoreleasepool {
-        return [QLMarkDownGenerator generateThumbnail:thumbnail forURL:url contentTypeUTI:contentTypeUTI options:options maxSize:maxSize];
-    }
-}
-
-void CancelThumbnailGeneration(void* thisInterface, QLThumbnailRequestRef thumbnail)
-{
-	//[QLMarkDownGenerator cancelThumbnailGeneration:thumbnail];
-	// implement only if supported
-}
-
-OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview,
-							   CFURLRef url, CFStringRef contentTypeUTI,
-							   CFDictionaryRef options)
-{
-    @autoreleasepool {
-        return [QLMarkDownGenerator generatePreview:preview forURL:url contentTypeUTI:contentTypeUTI options:options];
-    }
-}
-
-void CancelPreviewGeneration(void* thisInterface, QLPreviewRequestRef preview)
-{
-	//[QLMarkDownGenerator cancelPreviewGeneration:preview];
-	// implement only if supported
 }
